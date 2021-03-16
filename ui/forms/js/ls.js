a5c14dec252cd216232d0cdd2477fa0c3dc76252
paulwallin <steely_paul@hotmail.com>
2021-03-16 14:03:35 +0200

    localstorage uppdateringar

---
+++ ui/forms/js/ls.js
@@ -0,0 +1,40 @@
+//localstorage test med JavaScript
+
+//ställer in ett objekt med namne userName och värdet Paul
+//localStorage.setItem('userName', 'Paul')
+//sparar userName i en variabel
+const userName = localStorage.getItem('userName')
+//använder värdet t.ex. till att skriva ut i HTML
+document.getElementById('output').innerHTML = userName
+
+//radera valt objekt med removeItem
+//localStorage.removeItem('userName')
+
+//radera alla objekt i localStorage med clear()
+//localStorage.clear() 
+
+function formToLocalStorage() {
+    //plockar värdet från greet-fältet
+    const greet = document.getElementById('greet').value 
+    
+    const lsEmail = document.getElementById('lsEmail').value 
+    //console.log(greet)
+    localStorage.greet = greet
+    localStorage.lsEmail = lsEmail
+}
+
+console.log(localStorage.getItem('greet'))
+
+if(localStorage.getItem('greet') !== null){
+    document.getElementById('greet').value = localStorage.getItem('greet')
+    document.getElementById('lsEmail').value = localStorage.getItem('lsEmail')
+} else {
+    document.getElementById('output').innerHTML = 'Standard greeting here'
+}
+
+function clearLS() {
+    //Ta bort all local storage
+    localStorage.clear()
+    //ladda om sidan
+    location.reload()
+}
